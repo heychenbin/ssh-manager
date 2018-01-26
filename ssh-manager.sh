09cfea3e044da8715ce6c15c3558e65b5ebc6793
Chenbin <chen.bin11@zte.com.cn>
2018-01-26 11:03:46 +0800

    remove cc command, use default to handle alias

--- ssh-manager.sh
+++ ssh-manager.sh
@@ -26,106 +26,106 @@ SSH_DEFAULT_PORT=22
 #================== Functions ================================================
 
 function exec_ping() {
-	case $(uname) in 
-		MINGW*)
-			ping -n 1 -i $PING_DEFAULT_TTL $@
-			;;
-		*)
-			ping -c1 -t$PING_DEFAULT_TTL $@
-			;;
-	esac
+    case $(uname) in
+        MINGW*)
+            ping -n 1 -i $PING_DEFAULT_TTL $@
+            ;;
+        *)
+            ping -c1 -t$PING_DEFAULT_TTL $@
+            ;;
+    esac
 }
 
 function test_host() {
-	exec_ping $* > /dev/null
-	if [ $? != 0 ] ; then
-		echo -n "["
-		cecho -n -red "KO"
-		echo -n "]"
-	else
-		echo -n "["
-		cecho -n -green "UP"
-		echo -n "]"
-	fi 
+    exec_ping $* > /dev/null
+    if [ $? != 0 ] ; then
+        echo -n "["
+        cecho -n -red "KO"
+        echo -n "]"
+    else
+        echo -n "["
+        cecho -n -green "UP"
+        echo -n "]"
+    fi
 }
 
 function separator() {
-	echo -e "----\t----\t----\t----\t----\t----\t----\t----"
+    # echo -e "----\t----\t----\t----\t----\t----\t----\t----"
+    echo -e "------------------------------------------------"
 }
 
 function list_commands() {
-	separator
-	echo -e "Availables commands"
-	separator
-	echo -e "$0 cc\t<alias> [username]\t\tconnect to server"
-	echo -e "$0 add\t<alias>:<user>:<host>:[port]\tadd new server"
-	echo -e "$0 del\t<alias>\t\t\t\tdelete server"
-	echo -e "$0 export\t\t\t\t\texport config"
+    separator
+    echo -e "Availables commands"
+    separator
+    echo -e "ssh-manager \t\t\t\t\tlist all servers"
+    echo -e "ssh-manager \t<alias> [username]\t\tconnect to server"
+    echo -e "ssh-manager add\t<alias>:<user>:<host>:[port]:[desc]\tadd new server"
+    echo -e "ssh-manager del\t<alias>\t\t\t\tdelete server"
+    echo -e "ssh-manager cp\t<alias>\t\t\t\tssh-copy-id to server"
+    echo -e "ssh-manager export\t\t\t\texport config"
+    echo -e "ssh-manager -h / --help\t\t\t\tprint this message"
 }
 
-function probe ()
-{
-	als=$1
-	grep -w -e $als $HOST_FILE > /dev/null
-	return $?
+function probe () {
+    als=$1
+    grep -w -e $als $HOST_FILE > /dev/null
+    return $?
 }
 
-function get_raw ()
-{
-	als=$1
-	grep -w -e $als $HOST_FILE 2> /dev/null
+function get_raw () {
+    als=$1
+    grep -w -e $als $HOST_FILE 2> /dev/null
 }
 
-function get_addr ()
-{
-	als=$1
-	get_raw "$als" | awk -F "$DATA_DELIM" '{ print $'$DATA_HADDR' }'
+function get_addr () {
+    als=$1
+    get_raw "$als" | awk -F "$DATA_DELIM" '{ print $'$DATA_HADDR' }'
 }
 
-function get_port ()
-{
-	als=$1
-	get_raw "$als" | awk -F "$DATA_DELIM" '{ print $'$DATA_HPORT'}'
+function get_port () {
+    als=$1
+    get_raw "$als" | awk -F "$DATA_DELIM" '{ print $'$DATA_HPORT'}'
 }
 
-function get_user ()
-{
-	als=$1
-	get_raw "$als" | awk -F "$DATA_DELIM" '{ print $'$DATA_HUSER' }'
+function get_user () {
+    als=$1
+    get_raw "$als" | awk -F "$DATA_DELIM" '{ print $'$DATA_HUSER' }'
 }
+
 function server_add() {
-	probe "$alias"
-	if [ $? -eq 0 ]; then
-		as	echo "$0: alias '$alias' is in use"
-	else
-		echo "$alias$DATA_DELIM$user" >> $HOST_FILE
-		echo "new alias '$alias' added"
-	fi
+    probe "$alias"
+    if [ $? -eq 0 ]; then
+        as  echo "$0: alias '$alias' is in use"
+    else
+        echo "$alias$DATA_DELIM$user" >> $HOST_FILE
+        echo "new alias '$alias' added"
+    fi
 }
 
 function cecho() {
-	while [ "$1" ]; do
-		case "$1" in 
-			-normal)        color="\033[00m" ;;
-			-black)         color="\033[30;01m" ;;
-			-red)           color="\033[31;01m" ;;
-			-green)         color="\033[32;01m" ;;
-			-yellow)        color="\033[33;01m" ;;
-			-blue)          color="\033[34;01m" ;;
-			-magenta)       color="\033[35;01m" ;;
-			-cyan)          color="\033[36;01m" ;;
-			-white)         color="\033[37;01m" ;;
-			-n)             one_line=1;   shift ; continue ;;
-			*)              echo -n "$1"; shift ; continue ;;
-		esac
-	shift
-	echo -en "$color"
-	echo -en "$1"
-	echo -en "\033[00m"
-	shift
+    while [ "$1" ]; do
+        case "$1" in
+            -normal)        color="\033[00m" ;;
+            -black)         color="\033[30;01m" ;;
+            -red)           color="\033[31;01m" ;;
+            -green)         color="\033[32;01m" ;;
+            -yellow)        color="\033[33;01m" ;;
+            -blue)          color="\033[34;01m" ;;
+            -magenta)       color="\033[35;01m" ;;
+            -cyan)          color="\033[36;01m" ;;
+            -white)         color="\033[37;01m" ;;
+            -n)             one_line=1;   shift ; continue ;;
+            *)              echo -n "$1"; shift ; continue ;;
+        esac
+    shift
+    echo -en "$color"
+    echo -en "$1"
+    echo -en "\033[00m"
+    shift
 done
 if [ ! $one_line ]; then
-	echo
+    echo
 fi
 }
 
@@ -140,75 +140,99 @@ if [ ! -f $HOST_FILE ]; then touch "$HOST_FILE"; fi
 
 # without args
 if [ $# -eq 0 ]; then
-	separator 
-	echo "List of availables servers for user $(whoami) "
-	separator
-	while IFS=: read label user ip port         
-	do    
-	test_host $ip
-	echo -ne "\t"
-	cecho -n -blue $label
-	echo -ne ' ==> '
-	cecho -n -red $user 
-	cecho -n -yellow "@"
-	cecho -n -white $ip
-	echo -ne ' -> '
-	if [ "$port" == "" ]; then
-		port=$SSH_DEFAULT_PORT
-	fi
-	cecho -yellow $port
-	echo
+    separator
+    echo "List of availables servers for user $(whoami) "
+    separator
+    while IFS=: read label user ip port desc
+    do
+    # test_host $ip
+    # echo -ne "\t"
+    cecho -n -blue $label
+    echo -ne '\t==>\t'
+    cecho -n -red $user
+    cecho -n -yellow "@"
+    cecho -n -green $ip
+    # echo -ne ' -> '
+    echo -ne ':'
+    if [ "$port" == "" ]; then
+        port=$SSH_DEFAULT_PORT
+    fi
+    cecho -magenta $port
+    echo -ne "\t"
+    cecho -blue $desc
+    echo
 done < $HOST_FILE
-
-list_commands
+echo
+# list_commands
 
 exit 0
 fi
 
 case "$cmd" in
-	# Connect to host
-	cc )
-		probe "$alias"
-		if [ $? -eq 0 ]; then
-			if [ "$user" == ""  ]; then
-				user=$(get_user "$alias")
-			fi
-			addr=$(get_addr "$alias")
-			port=$(get_port "$alias")
-			# Use default port when parameter is missing
-			if [ "$port" == "" ]; then
-				port=$SSH_DEFAULT_PORT
-			fi
-			echo "connecting to '$alias' ($addr:$port)"
-			ssh $user@$addr -p $port
-		else
-			echo "$0: unknown alias '$alias'"
-			exit 1
-		fi
-		;;
-
-	# Add new alias
-	add )
-		server_add
-		;;
-	# Export config
-	export )
-		echo
-		cat $HOST_FILE
-		;;
-	# Delete ali
-	del )
-		probe "$alias"
-		if [ $? -eq 0 ]; then
-			cat $HOST_FILE | sed '/^'$alias$DATA_DELIM'/d' > /tmp/.tmp.$$
-			mv /tmp/.tmp.$$ $HOST_FILE
-			echo "alias '$alias' removed"
-		else
-			echo "$0: unknown alias '$alias'"
-		fi
-		;;
-	* )
-		echo "$0: unrecognised command '$cmd'"
-		exit 1
-		;;
+    # Add new alias
+    add )
+        server_add
+        ;;
+    # Export config
+    export )
+        echo
+        cat $HOST_FILE
+        ;;
+    # Delete ali
+    del )
+        probe "$alias"
+        if [ $? -eq 0 ]; then
+            cat $HOST_FILE | sed '/^'$alias$DATA_DELIM'/d' > /tmp/.tmp.$$
+            mv /tmp/.tmp.$$ $HOST_FILE
+            echo "alias '$alias' removed"
+        else
+            echo "$0: unknown alias '$alias'"
+        fi
+        ;;
+    -h )
+        list_commands
+        ;;
+    --help )
+        list_commands
+        ;;
+    cp )
+        probe "$alias"
+        if [ $? -eq 0 ]; then
+            if [ "$user" == ""  ]; then
+                user=$(get_user "$alias")
+            fi
+            addr=$(get_addr "$alias")
+            port=$(get_port "$alias")
+            # Use default port when parameter is missing
+            if [ "$port" == "" ]; then
+                port=$SSH_DEFAULT_PORT
+            fi
+            echo "connecting to '$alias' ($addr:$port)"
+            ssh-copy-id $user@$addr -p $port
+        else
+            echo "$0: unknown alias '$alias'"
+            exit 1
+        fi
+        ;;
+    # Connect to host or unknown cmd...
+    * )
+        alias=$cmd
+        probe "$alias"
+        if [ $? -eq 0 ]; then
+            if [ "$user" == ""  ]; then
+                user=$(get_user "$alias")
+            fi
+            addr=$(get_addr "$alias")
+            port=$(get_port "$alias")
+            # Use default port when parameter is missing
+            if [ "$port" == "" ]; then
+                port=$SSH_DEFAULT_PORT
+            fi
+            echo "connecting to '$alias' ($addr:$port)"
+            ssh $user@$addr -p $port
+        else
+            echo "$0: unknown cmd '$alias'"
+            exit 1
+        fi
+        ;;
 esac
